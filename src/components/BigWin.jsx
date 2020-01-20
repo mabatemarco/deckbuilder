5c97b7a5ad796067f85f9d0a457eabf4bfc2f279
mabatemarco <mabatemarco928@gmail.com>
2020-01-20 13:18:47 -0500

    winimage

--- src/components/BigWin.jsx
+++ src/components/BigWin.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Winner from '../images/characterwin.jpg'
 
 const BigWin = (props) => {
-  return(
-  <div className="bigWin">
+  return (
+    <div className="bigWin">
       <h1>You Did It!</h1>
-      <img src={props.gif} alt=""/>
-    <button onClick={props.newGame}>Play Again</button>
+      <img src={Winner} alt="" />
+      <button onClick={props.newGame}>Play Again</button>
     </div>
   )
 }
